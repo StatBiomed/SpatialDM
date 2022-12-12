135fd22248aa44fd5f3b99c8b0a0b898ddd758b9
leeyoyohku <leeyoyo@hku.hk>
2022-12-12 01:21:28 +0000

    wrapped into an adata object"

--- spatialdm/main.py
+++ spatialdm/main.py
@@ -7,7 +7,7 @@ from scipy import spatial
 import json
 from threadpoolctl import threadpool_limits
 from scipy.sparse import csc_matrix, save_npz, load_npz
-from utils import * #TODO .utils
+from .utils import * #TODO .utils
 from itertools import zip_longest
 
 
