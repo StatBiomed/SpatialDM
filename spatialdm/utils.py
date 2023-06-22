1688930abda73cb7f8358d27759384c6acbe3945
Yuanhua Huang <huangyh094@gmail.com>
2023-06-22 16:43:39 +0800

    add pure Moran R function

--- spatialdm/utils.py
+++ spatialdm/utils.py
@@ -10,6 +10,70 @@ from tqdm import tqdm
 from scipy.sparse import csc_matrix, csr_matrix, issparse, hstack
 
 
+# pure statistics for bivariate Moran's R
+def Moran_R_std(spatial_W, by_trace=False):
+    """Calculate standard deviation of Moran's R under the null distribution.
+    """
+    N = spatial_W.shape[0]
+    
+    if by_trace:
+        W = spatial_W.copy()
+        H = np.identity(N) - np.ones((N, N)) / N
+        HWH = H.dot(W.dot(H))
+        var = np.trace(HWH.dot(HWH)) * N**2 / (np.sum(W) * (N-1))**2
+    else:
+        if issparse(spatial_W):
+            nm = N ** 2 * spatial_W.multiply(spatial_W.T).sum() \
+                - 2 * N * (spatial_W.sum(0) @ spatial_W.sum(1)).sum() \
+                + spatial_W.sum() ** 2
+        else:
+            nm = N ** 2 * (spatial_W * spatial_W.T).sum() \
+                - 2 * N * (spatial_W.sum(1) * spatial_W.sum(0)).sum() \
+                + spatial_W.sum() ** 2
+        dm = N ** 2 * (N - 1) ** 2
+        var = nm / dm
+    
+    return np.sqrt(var)
+
+
+def Moran_R(X, Y, spatial_W, standardise=True, nproc=1):
+    """Computing Moran's R for pairs of variables
+    
+    :param X: Variable 1, (n_sample, n_variables) or (n_sample, )
+    :param Y: Variable 2, (n_sample, n_variables) or (n_sample, )
+    :param spatial_W: spatial weight matrix, sparse or dense, (n_sample, n_sample)
+    :param nproc: default to 1. Numpy may use more without much speedup.
+    
+    :return: (Moran's R, z score and p values)
+    """
+    if len(X.shape) < 2:
+        X = X.reshape(-1, 1)
+    if len(Y.shape) < 2:
+        Y = Y.reshape(-1, 1)
+        
+    if standardise:
+        X = (X - np.mean(X, axis=0, keepdims=True)) / np.std(X, axis=0, keepdims=True)
+        Y = (Y - np.mean(Y, axis=0, keepdims=True)) / np.std(Y, axis=0, keepdims=True)
+        
+    # Consider to dense array for speedup (numpy's codes is optimised)
+    if X.shape[0] <= 5000 and issparse(spatial_W):
+        # Note, numpy may use unnessary too many threads
+        # You may use threadpool.threadpool_limits() outside
+        from threadpoolctl import threadpool_limits
+        
+        with threadpool_limits(limits=nproc, user_api='blas'):
+            R_val = (spatial_W.A @ X * Y).sum(axis=0) / np.sum(spatial_W)
+    else:
+        # we assume it's sparse spatial_W when sample size > 5000
+        R_val = (spatial_W @ X * Y).sum(axis=0) / np.sum(spatial_W)
+        
+    _R_std = Moran_R_std(spatial_W)
+    R_z_score = R_val / _R_std
+    R_p_val = stats.norm.sf(R_z_score)
+    
+    return R_val, R_z_score, R_p_val
+
+
 # global variance
 def globle_st_compute(adata):
     N = adata.shape[0]
